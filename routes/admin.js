5451caf99678258a56fa79f6d374ea98ca51d578
yhj129 <y.haejii@gmail.com>
2025-07-16 17:55:52 +0900

    07-16, isAuth 코드 수정

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 // server/routes/admin.js
 import express from 'express';
-import authMiddleware from '../middlewares/authMiddleware.js';
+import { isAuth } from '../middlewares/authMiddleware.js';
 import isAdmin from '../middlewares/isAdmin.js';
 import upload from '../middlewares/cloudinaryUploader.js';
 import { getDashboardStats, getAllUsers } from '../controllers/adminController.js';
 
 const router = express.Router();
 
-router.use(authMiddleware, isAdmin); // 인증 + 관리자 권한 필수
+router.use(isAuth, isAdmin); // 인증 + 관리자 권한 필수
 
 router.get('/dashboard', getDashboardStats);
 router.get('/users', getAllUsers);

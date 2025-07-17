b2986d7aced98048e1349a349c1f12956353c795
yhj129 <y.haejii@gmail.com>
2025-07-17 14:32:48 +0900

    07-17, server index.js 수정

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 // server/index.js
-/*
-import express from 'express';
-
-const app = express();
-const port = 3001;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
-*/
-// server/index.js
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -29,28 +14,40 @@ const app = express();
 
 // CORS 설정
 const whitelist = [
-  'http://localhost:5173',                   // 로컬 개발 주소
-  'https://eduai-react-v3-fm-gamma.vercel.app'     // 배포된 프론트엔드 주소
+  'http://localhost:5173',
+  'https://eduai-react-v3-fm.vercel.app'
 ];
-app.use(cors({ origin: whitelist, credentials: true }));
+// app.use(cors({ origin: whitelist, credentials: true }));
 
-// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-// app.use(cors({ origin: 'https://eduai-react-v3-fm-gamma.vercel.app', credentials: true }));
+// ✅✅✅ 추가1
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
 app.use(express.json());
 
-// 라우터 연결
+// 라우터 등록
 app.use('/api/auth', authRoutes);
-app.use('/api', courseRoutes); // /api/courses, /api/favorites
+app.use('/api', courseRoutes);
 
-// ✅추가 업로드 라우터 연결
-app.use('/api/admin', adminRoutes); // admin경로요청시  adminRoutes작동
 
-// DB 연결 후 서버 실행
+// app.use('/api', uploadRoutes);
+app.use('/api/admin', adminRoutes); //✅✅✅ admin경로요청시  adminRoutes작동
+
+// DB 연결 및 서버 실행
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('✅ MongoDB 연결 성공');
+    console.log(' MongoDB 연결 성공');
     app.listen(5000, () => {
-      console.log('✅ 서버 실행 중: http://localhost:5000');
+      console.log('서버 실행 중: http://localhost:5000');
     });
   })
   .catch((err) => console.error('❌ DB 연결 실패:', err));

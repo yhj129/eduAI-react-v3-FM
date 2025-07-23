90697ee49e286dcff40573fa693f76b5ed639f2a
yhj129 <y.haejii@gmail.com>
2025-07-23 13:31:46 +0900

    admin.js 코드 수정

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 // server/routes/admin.js
 import express from 'express';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
+import {authMiddleware} from '../middlewares/authMiddleware.js';
 import isAdmin from '../middlewares/isAdmin.js';
-import upload from '../middlewares/cloudinaryUploader.js';
-import { getDashboardStats, getAllUsers, uploadImage, getImages } from '../controllers/adminController.js';
+import upload from '../middlewares/cloudinaryUploader.js'; // ✅← 추가할 부분
+import { getDashboardStats, getAllUsers,uploadImage, getImages, deleteImage } from '../controllers/adminController.js';  // ✅✅ ← deleteImage 추가할 부분
 
 const router = express.Router();
 
@@ -11,7 +11,9 @@ router.use(authMiddleware, isAdmin); // 인증 + 관리자 권한 필수
 
 router.get('/dashboard', getDashboardStats);
 router.get('/users', getAllUsers);
-router.post('/upload-image', upload.single('file'), uploadImage); 
-router.get('/images', getImages);   
+router.post('/upload-image', upload.single('file'), uploadImage);         // ✅← 추가할 부분
+router.get('/images', getImages);                  // ✅ ← 추가할 부분
+
+router.post('/delete-image', deleteImage); // ✅✅ ←  POST 요청으로 업로드 된 이미지 삭제
 
 export default router;

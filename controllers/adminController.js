4754d7ac0e2c3373323e080048655670d405ea6c
yhj129 <y.haejii@gmail.com>
2025-07-23 13:30:49 +0900

    업로드 삭제 추가

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -33,17 +33,49 @@ export const uploadImage = async (req, res) => {
   }
 };
 
-// ✅✅✅ 이미지 목록 불러오기 컨트롤러
-export const getImages = async (req, res) => {
+// ✅✅✅ 이미지 목록 cloudinary에서 불러오기
+// export const getImages = async (req, res) => {
+//   try {
+//     const result = await cloudinary.api.resources({
+//       type: 'upload',
+//       prefix: 'admin-uploads/',
+//       max_results: 30,
+//       sort_by: 'created_at:desc', // ← 추가 옵션
+//     });
+//     res.json(result.resources); // 배열 반환
+//   } catch (err) {
+//     res.status(500).json({ error: 'Cloudinary 이미지 조회 실패' });
+//   }
+// };
+
+// 서버: MongoDB에서 이미지 목록 조회
+export const getImageList = async (req, res) => {
   try {
-    const result = await cloudinary.api.resources({
-      type: 'upload',
-      prefix: 'admin-uploads/',
-      max_results: 30,
-      sort_by: 'created_at:desc', // ← 추가 옵션
-    });
-    res.json(result.resources); // 배열 반환
+  
+    // ✅✅ 수정된 MongoDB 조회 방식
+    const images = await Image.find().sort({ createdAt: -1 }); // 최신순 정렬
+    console.log('📦 MongoDB에서 불러온 이미지 수:', images.length); // ✅ 개수 로그
+    res.json(images); // 배열 반환
+    
   } catch (err) {
-    res.status(500).json({ error: 'Cloudinary 이미지 조회 실패' });
+    res.status(500).json({ error: '이미지 목록 불러오기 실패' });
+  }
+};
+
+// ✅이미지 삭제 컨트롤러
+export const deleteImage = async (req, res) => {
+  const { public_id } = req.body;
+  console.log('🗑 삭제 요청됨: ', public_id); // ← 로그 추가
+  try {
+    // ✅ 1. Cloudinary에서 이미지 삭제
+    const result = await cloudinary.uploader.destroy(public_id);
+
+    // ✅ 2. MongoDB에서도 해당 이미지 문서 삭제
+    await Image.findOneAndDelete({ public_id });
+
+    res.json({ success: true, result });
+  } catch (err) {
+    console.error('❌ 이미지 삭제 실패:', err);
+    res.status(500).json({ success: false, message: '이미지 삭제 실패', detail: err.message });
   }
 };

4585199d711943e6e726b7cd7c341b8fd2e2ff30
yhj129 <y.haejii@gmail.com>
2025-07-23 13:52:33 +0900

    재업로드

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 // server/index.js
-/*
-import express from 'express';
-
-const app = express();
-const port = 3001;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
-*/
-// server/index.js
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
